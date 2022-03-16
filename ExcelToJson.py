a8fe3610864502f9832dba5657c92ea10af5f50e
Tahseem-Ahmad <tahseem.ahmad62@gmail.com>
2022-03-17 00:42:19 +0530

    Add files via upload

--- ExcelToJson.py
+++ ExcelToJson.py
@@ -1,52 +1,42 @@
 import pandas as pd
 import json as json
+import simplejson as sj
+from datetime import datetime
 
-excel_data_df = pd.read_excel('E:/TABLE_COLUMNS.xlsx', sheet_name='Sheet1')
-print(type(excel_data_df))
-json_struct = {
-    "RoleName": "",
-    "Active": 0,
-    "InactiveDate": "",
-    "LHCode": "LH001",
-    "AuditRecInsertDate": "2/07/2022",
-    "Env": "Lab",
-    "Roleid": 0,
-    "RoleInfo": {
-        "authorization": {
-            "bcbsa_bcp_edar_cg": {
-                "fg_groups": [
-                    "edar_std"
-                ],
-                "ndw_plan_code": [
-                    "425"
-                ]
-            }
-        },
-        "version": "0.0.1",
-        "lh_code": "LH001"
-    },
-    "AuditRecInsertBy": "",
-    "AuditRecUpdby": "",
-    "LHEntityCode": "",
-    "AuditRecUpDate": ""
-}
-json_array = list()
+excel_data_df = pd.read_excel('E:/TABLE_COLUMNS.xlsx', sheet_name='Sheet1', dtype=str)
+# print(type(excel_data_df))
+
+json_list = []
+
+
+def dateToStr(param):
+    if type(param) == str:
+        yyyy, mm, dd = param.split(" ")[0].split("-")[0], param.split(" ")[0].split("-")[1], \
+                       param.split(" ")[0].split("-")[2]
+        return dd + '/' + mm + '/' + yyyy
+
 
 for ind in excel_data_df.index:
-    json_struct['RoleName'] = excel_data_df['RoleName'][ind]
-    json_struct['Active'] = excel_data_df['Active'][ind]
-    json_struct['InactiveDate'] = pd.to_datetime(excel_data_df['InactiveDate'][ind], unit='ms')
-    json_struct['LHCode'] = excel_data_df['LHEntityCode'][ind].split("-")[0]
-    json_struct['AuditRecInsertDate'] = pd.to_datetime(excel_data_df['RecordInsertDate'][ind], unit='ms')
-    json_struct['Env'] = excel_data_df['Env'][ind]
-    json_struct['Roleid'] = excel_data_df['RoleID'][ind]
-    json_struct['AuditRecInsertBy'] = excel_data_df['RecordInsertedBy'][ind]
-    json_struct['AuditRecUpdby'] = excel_data_df['RecordUpdatedBy'][ind]
-    json_struct['LHEntityCode'] = excel_data_df['LHEntityCode'][ind]
-    json_struct['AuditRecUpDate'] = pd.to_datetime(excel_data_df['RecordUpdateDate'], unit='ms')
-    json_array.append(json_struct)
+    json_struct = {'RoleName': excel_data_df['RoleName'][ind],
+                   'Active': excel_data_df['Active'][ind],
+                   'InactiveDate': dateToStr(excel_data_df['InactiveDate'][ind]),
+                   'LHCode': excel_data_df['LHEntityCode'][ind].split("-")[0],
+                   'AuditRecInsertDate': dateToStr(excel_data_df['RecordInsertDate'][ind]),
+                   'Env': excel_data_df['Env'][ind],
+                   'Roleid': excel_data_df['RoleID'][ind],
+                   'RoleInfo': {'authorization': {}, 'version': '0.0.1',
+                                'lh_code': excel_data_df['LHEntityCode'][ind].split("-")[0]},
+                   'AuditRecInsertBy': excel_data_df['RecordInsertedBy'][ind],
+                   'AuditRecUpdby': excel_data_df['RecordUpdatedBy'][ind],
+                   'LHEntityCode': excel_data_df['LHEntityCode'][ind],
+                   'AuditRecUpDate': dateToStr(excel_data_df['RecordUpdateDate'][ind])}
+    # print(ind)
+    auth = excel_data_df['CoarsegrainedGroup'][ind]
+    auth = {auth: {"fg_groups": [excel_data_df['FineGrainedGroup'][ind]],
+                   "ndw_plan_code": [excel_data_df['NDWPlanCode'][ind]]}}
+    json_struct['RoleInfo']['authorization'] = auth
+    json_list.append(json_struct)
 
-# json_str = excel_data_df.to_json(orient='records')
+json_array = sj.dumps(json_list, ignore_nan=True)
+
 print(json_array)
-json_struct['RoleInfo']['authorization']['bcbsa_bcp_edar_cg']['ndw_plan_code'] = ["430"]
-print(json_struct['RoleInfo']['authorization']['bcbsa_bcp_edar_cg']['ndw_plan_code'])
